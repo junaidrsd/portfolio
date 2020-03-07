9b0dcef047b3677747be9880c3feeb379ddeedac
Junaid Rasheed <junaidrsd@hotmail.com>
2020-03-07 22:24:57 +0200

    Updates

---
+++ precache-manifest.5d74035272f3f9bed1ad113ed2cc2856.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60570e413d420af16a75cba2a78096d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "989c15c567eb12eb647d",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1165d37a72ec82789edb",
+    "url": "/portfolio/static/css/main.9e358bdf.chunk.css"
+  },
+  {
+    "revision": "989c15c567eb12eb647d",
+    "url": "/portfolio/static/js/2.c56b15a2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.c56b15a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1165d37a72ec82789edb",
+    "url": "/portfolio/static/js/main.5f0623b3.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  },
+  {
+    "revision": "2556d7a29ab01dc80b96eb824a049df6",
+    "url": "/portfolio/static/media/Effra_Std_Rg.2556d7a2.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.99966d26592117118ddacaa936eaa559.js"
+  "/portfolio/precache-manifest.5d74035272f3f9bed1ad113ed2cc2856.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
