ffb250ad9377682cbfa2d94d5db5a8d13d16f2e1
Junaid Rasheed <junaidrsd@hotmail.com>
2020-03-08 12:53:11 +0200

    project details can be fetched now

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import projects from '../../shared/projects.json';
 
-const Project = () => (
-  <div className="project">
-    This is project
-  </div>
-)
+const Project = ({ match }) => {
+  const matchedProject = projects.filter( project => project.id === match.params.id)[0];
+  return(
+    <div className="project">
+      {matchedProject.title}
+    </div>
+  )
+}
 
 export default Project;
